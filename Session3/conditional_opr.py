d3958206b68084593f932ed4ab62e13618b37b6c
RadhikaK14 <62436185+RadhikaK14@users.noreply.github.com>
2021-06-02 13:57:39 +0100

    Update conditional operators

--- Session3/conditional_opr.py
+++ Session3/conditional_opr.py
@@ -1,8 +1,12 @@
 #1a
 shirt_price = float(input('Please enter the price of the shirt\n'))
+shirt_in_budget = shirt_price <=25.00
+
+print(f'Shirt is available within budget: {shirt_in_budget}')
+
+#1b
+shirt_price = float(input('Please enter the price of the shirt\n'))
 shirt_colour = input('What is its colour?\n')
-shirt_in_budget = (shirt_price <=25.00 and shirt_colour.lower() == 'yellow')
+shirt_in_budget_and_colour = (shirt_price <=25.00 and shirt_colour.lower() == 'yellow')
 
-print(f'Shirt is available within budget and correct colour: {shirt_in_budget}')
-
-
+print(f'Shirt is available within budget and required colour: {shirt_in_budget_and_colour}')
