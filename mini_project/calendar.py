f39e0e55ed41d089be405bbffe26fa92bd0afbc0
RadhikaK14 <62436185+RadhikaK14@users.noreply.github.com>
2021-06-12 17:09:10 +0100

    Mini_project 'Calendar Generator'

--- mini_project/calendar.py
+++ mini_project/calendar.py
@@ -1,5 +1,5 @@
-# starting_day = input('Please enter a starting day [Monday 1-7 Sunday]:')
-Num_of_days = input('Please enter number of days in the month [28-31] : ')
+start_day = int(input('Please enter a starting day [Monday 1-7 Sunday]:'))
+Num_of_days = int(input('Please enter number of days in the month [28-31] : '))
 
 print('Mo\tTu\tWe\tTh\tFr\tSa\tSu\n')
 
@@ -8,5 +8,21 @@ for i in range(1, int(Num_of_days)+1):
     if i % 7 == 0:
         print('\n')
     
+    print('Mo\tTu\tWe\tTh\tFr\tSa\tSu\n')
+
+    i=1
+    print('\t' * (start_day-1), i, end= '\t')
+    for i in range(2, Num_of_days+1):
+        if (i + start_day == 9):
+            print('\n')
+        print(i, end = '\t')
+        if (i > 7) and ((i+ start_day-1) % 7) == 0:
+            print('\n')
+else:
+    print('Please enter a valid number')
 
 
+# for i in range(1, int(Num_of_days)+1):
+#     print(i, end= '\t')
+#     if i % 7 == 0:
+#         print('\n')
