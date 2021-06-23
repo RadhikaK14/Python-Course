3de74f357cb7c346fbba54d9703bc3b08e0be301
RadhikaK14 <62436185+RadhikaK14@users.noreply.github.com>
2021-06-23 20:35:23 +0100

    Improve the readability of the project

--- Final_Project/Guess_the_word.py
+++ Final_Project/Guess_the_word.py
@@ -1,36 +1,47 @@
 import random
 
-word=['hello', 'python', 'testing', 'coding', 'variable', 'function', 'module']
-word_count= len(word)
-choice = random.randint(0, word_count-1)
-guess_word = word[choice]
-# print(choice)
-# print(guess_word)
-ans=[]
-length = len(guess_word)
-for i in range(length):
-    ans.append("_")
+def guess_word():
+    my_list=['hello', 'python', 'testing', 'coding', 'variable', 'function', 'module']
+    choice = random.randint(0, len(my_list)-1)
+    word = my_list[choice]
+    return word
+    # print(guess_word)
 
-print(' '.join(ans))
+def ans():
+    ans=[]
+    word = guess_word()
+    length = len(word)
+    for i in range(length):
+        ans.append("_")
+    print(' '.join(ans))
+    return ans
 
-attempts = 0    
-while (ans != list(guess_word)) and (attempts < 5):
-    guess_letter = input("Enter a letter: ")
-    if len(guess_letter) > 1:
-        print('Error: You have entered more than 1 letter')
-        attempts = attempts + 1
-    else:
-        if guess_letter in guess_word:
+def main():
+    attempts = 0
+    user_ans = ans()
+    word = guess_word()
+
+    while (user_ans != list(word)) and (attempts < 5):
+        guess_letter = input("Enter a letter: ")
+
+        if (len(guess_letter) == 1) and (guess_letter in word):
             # enumerate function assigns each letter with an index. Ex: enumerate['h','e','l','l','o']= (0 h, 1 e, 2 l, 3 l, 4 o)
-            for position,l in enumerate(guess_word):
+            for position,l in enumerate(word):
                 if l == guess_letter:
-                    ans[position]= guess_letter
+                    user_ans[position]= guess_letter
+
+        elif len(guess_letter) > 1:
+            attempts = attempts + 1
+            print('Error: You have entered more than 1 letter')
+        
         else:
             attempts = attempts + 1
-    
-    print(' '.join(ans))
+        
+        print(' '.join(user_ans))
 
-if attempts >= 5:
-    print('Sorry, better luck next time!')
-else:
-    print('Yayy, u guessed it right!')
+    if attempts >= 5:
+        print('Sorry, better luck next time!')
+    else:
+        print('Yayy, u guessed it right!')
+
+main()
